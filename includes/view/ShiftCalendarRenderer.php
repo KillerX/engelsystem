05e866208a5565bedf5897bb27f91e0f1c99028d
Matjaz Debelak <matjaz@debelak.org>
2022-09-17 23:15:40 +0200

    More angels

--- includes/view/ShiftCalendarRenderer.php
+++ includes/view/ShiftCalendarRenderer.php
@@ -321,7 +321,7 @@ class ShiftCalendarRenderer
         return div('legend mt-3', [
             badge(__('Your shift'), 'primary'),
             badge(__('Help needed'), 'danger'),
-            badge(__('Other angeltype needed / collides with my shifts'), 'warning'),
+            badge(__('Other usertypes needed / collides with my shifts'), 'warning'),
             badge(__('Shift is full'), 'success'),
             badge(__('Shift running/ended or user not arrived/allowed'), 'secondary')
         ]);
